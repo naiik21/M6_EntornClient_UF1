b7260bb20267566656eb0e0a28a5a83d874169da
naiik21 <nratia22@jaumebalmes.net>
2023-10-02 17:20:07 +0200

    prcatica actualizada de cambio de bases

--- JS/11-Base.js
+++ JS/11-Base.js
@@ -1,58 +1,38 @@
-let num = parseInt(prompt("Diguem un numero: "));
-
-
-function binari(num){
-    if(num===0){
-        console.log(num + " en base 10 es 0 en binari");
-    }else if(num<0){
-        console.log("ERROR");
-    }else{
-        let aux=num;
-    let residu=0;
-    let result="";
-    while(aux>0){
-        residu=Math.floor(aux%2);
-        aux= Math.floor(aux/2);
-        result= residu.toString() + result;
-    }
-    console.log(num + " en base 10 es " + result + " en binari");
-    }
-    
-}
-
-function octal(num){
-    if(num===0){
-        console.log(num + " en base 10 es 0 en octal");
-    }else if(num<0){
-        console.log("ERROR");
-    }else{
-    let aux=num;
-    let residu=0;
-    let result="";
-    while(aux>0){
-        residu=Math.floor(aux%8);
-        aux= Math.floor(aux/8);
-        result= residu.toString() + result;
-    }
-    console.log(num + " en base 10 es " + result + " en octal");
-}
-}
-
-function hexadecimal(num){
-    if(num===0){
-        console.log(num + " en base 10 es 0 en hexadecimal");
-    }else if(num<0){
-        console.log("ERROR");
-    }else{
+function calculadoraBase(){
+    let num = parseInt(prompt("Diguem un numero: "));
+    let tipus="";
+    let divisor=0;
+    let base="";
     let aux=num;
     let residu=0;
     let result="";
     let nums= ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"];
-    while(aux>0){
-        residu=Math.floor(aux%16);
-        aux= Math.floor(aux/16);
-        result= nums[residu] + result;
+    
+    do{
+        tipus= prompt("Diguem si vols pasar-ho a binaria amb una 'B', octal amb una 'O' o hexadecimal amb una 'H': " );
+    }while(!(tipus.toUpperCase()=="B" || tipus.toUpperCase()=="O" || tipus.toUpperCase()=="H"))
+    
+    if(tipus=="B"){
+        divisor=2;
+        base="binari";
+    }else if(tipus=="O"){
+        divisor=8;
+        base="octal";
+    }else{
+        divisor=16;
+        base="hexadecimal";
     }
-    console.log(num + " en base 10 es " + result + " en hexadecimal");
+
+    if(num===0){
+        console.log(num + " en base 10 es 0 en " + base);
+    }else if(num<0){
+        console.log("ERROR");
+    }else{
+        while(aux>0){
+            residu=Math.floor(aux%divisor);
+            aux= Math.floor(aux/divisor);
+            result= nums[residu] + result;
+        }
+        console.log(num + " en base 10 es " + result + " en " + base);
     }
 }
